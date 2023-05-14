97448059e542b26d93cc6dc43c550bbc07c1320b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-14 12:04:37 -0300

    Criação de todos elementos no HTML para manipula-los no JS

--- javaScript/recomendacao.js
+++ javaScript/recomendacao.js
@@ -1,20 +1,13 @@
-let script = document.querySelector("script");
-let divCaixa = document.body;
-let divDepoimento = document.createElement("div");
-divCaixa.insertBefore(divDepoimento, script);
+let foto = document.getElementById("foto");
+foto.src = "imagens/camila-pessoa.jpeg";
 
 let pessoas = [];
 
 let pessoa = {
-	"nome": "Camila Pessôa",
-	"cargo": "Desenvolvedora",
-	"recomendacao": "Lucas é alguém incrivelmente dedicado. Tem autonomia para solucionar problemas e é apaixonado por tecnologia. Além disso possui um grande carisma e consegue conviver em harmonia com diferentes pessoas. Suas habilidades técnicas também evoluem a cada commit e projeto, é maravilhoso acompanhar seu desempenho!"
+	foto : "../imagens/camila-pessoa.jpeg",
+	nome : "Camila Pessôa",
+	cargo : "Desenvolvedora",
+	depoimento : "Lucas é alguém incrivelmente dedicado. Tem autonomia para solucionar problemas e é apaixonado por tecnologia. Além disso possui um grande carisma e consegue conviver em harmonia com diferentes pessoas. Suas habilidades técnicas também evoluem a cada commit e projeto, é maravilhoso acompanhar seu desempenho!"
 }
 
-var nome = document.createTextNode(pessoa.nome);
-
-divDepoimento.appendChild(nome);
-
 pessoas.push(pessoa);
-
-console.log(pessoas);
